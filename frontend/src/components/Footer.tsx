bb5619ee249b537e110a7d3939bddee77ce791d9
Martionex <jordimartijnvanos@gmail.com>
2022-10-17 23:20:25 +0200

    Deployment adjustments
    Adds docker-compose file without nginx
    Updates prod docker compose
    Updates prod nginx configuration
    Adds dockerignore to the frontend
    Adds correct image open graph elements to articles and afdelingen
    Updates docker and nginx configuration
    Adds more Umami click actions to track
    Updates the README

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -11,17 +11,17 @@ export default function Footer() {
             </div>
             <div className="flex flex-col gap-2">
                 <a href="mailto:info@roodjongeren.nl"
-                   className="hover:underline">info@roodjongeren.nl</a>
+                   className="hover:underline umami--click--footer-email">info@roodjongeren.nl</a>
                 <Link href="/privacybeleid">
                     <a className="hover:underline">Privacybeleid</a>
                 </Link>
                 <a href="https://mijn.roodjongeren.nl"
-                   className="hover:underline">
+                   className="hover:underline umami--click--footer-mijnrood">
                     mijnROOD
                 </a>
                 <p>KVK: 24349986</p>
                 <a href="https://www.google.nl/maps/place/Gaffelstraat+61B,+3014+RC+Rotterdam"
-                   className="hover:underline">
+                   className="hover:underline umami--click--footer-adres">
                     <p>Gaffelstraat 61B</p>
                     <p>3014 RC Rotterdam</p>
                 </a>
@@ -29,27 +29,27 @@ export default function Footer() {
             <div className="flex flex-col gap-2">
                 <IconContext.Provider value={{className: 'inline mr-2 origin-center group-hover:scale-125 transition-transform'}}>
                     <a href="https://twitter.com/ROODjong"
-                       className="hover:underline group">
+                       className="hover:underline group umami--click--footer-twitter">
                         <FaTwitter/>Twitter
                     </a>
                     <a href="https://www.facebook.com/ROODjongSP"
-                       className="hover:underline group">
+                       className="hover:underline group umami--click--footer-facebook">
                         <FaFacebook/>Facebook
                     </a>
                     <a href="https://www.instagram.com/roodsocialistischejongeren"
-                       className="hover:underline group">
+                       className="hover:underline group umami--click--footer-instagram">
                         <FaInstagram/>Instagram
                     </a>
                     <a href="https://www.tiktok.com/@roodjong"
-                       className="hover:underline group">
+                       className="hover:underline group umami--click--footer-tiktok">
                         <FaTiktok/>Tiktok
                     </a>
                     <a href="https://github.com/roodjong"
-                       className="hover:underline group">
+                       className="hover:underline group umami--click--footer-github">
                         <FaGithub/>GitHub
                     </a>
                     <a href="https://reddit.com/r/Poldersocialisme/"
-                       className="hover:underline group">
+                       className="hover:underline group umami--click--footer-reddit">
                         <FaReddit/>Reddit
                     </a>
                 </IconContext.Provider>
