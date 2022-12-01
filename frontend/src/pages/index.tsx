d3c85be50ef891d9714da355130689ddf2f91346
Jelle Besseling <jelle@pingiun.com>
2022-12-01 12:47:47 +0100

    Stylized groups map

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,16 @@
-import Banner from '../components/Banner';
-import AboutUsShort from '../components/AboutUsShort';
-import dynamic from 'next/dynamic';
-import {useMemo} from 'react';
-import {fetchAfdelingen, fetchHome, fetchPosts} from '../utils/backend';
-import Afdeling from '../models/Afdeling';
-import HomeContent from '../models/HomeContent';
-import {Post, PostType} from '../models/Post';
-import Main from '../components/Main';
-import {revalidate} from '../utils/revalidate';
-import {GetStaticPropsResult} from 'next';
-import Subheader from '../components/Subheader';
-import EndlessPostsLoader from '../components/EndlessPostsLoader';
-import HeadPage from '../components/HeadPage';
+import Banner from "../components/Banner";
+import AboutUsShort from "../components/AboutUsShort";
+import { fetchAfdelingen, fetchHome, fetchPosts } from "../utils/backend";
+import Afdeling from "../models/Afdeling";
+import HomeContent from "../models/HomeContent";
+import { Post, PostType } from "../models/Post";
+import Main from "../components/Main";
+import { revalidate } from "../utils/revalidate";
+import { GetStaticPropsResult } from "next";
+import Subheader from "../components/Subheader";
+import EndlessPostsLoader from "../components/EndlessPostsLoader";
+import HeadPage from "../components/HeadPage";
+import AfdelingenTable from "../components/AfdelingenTable";
 
 interface Props {
     homeContent: HomeContent;
@@ -22,28 +21,58 @@ interface Props {
 
 export default function HomePage(props: Props) {
     const content = props.homeContent;
-    const AfdelingenMap = useMemo(() => dynamic(() => import('../components/AfdelingenMap'), {ssr: false}), []);
-    
-    return <div>
-        <HeadPage title="ROOD, Socialistische Jongeren"
-                  description="Politieke organisatie voor socialistische of anderzijds linkse jongeren"
-                  url="https://roodjongeren.nl/"/>
-        <Banner title={content.bannerTitle} subtitle={content.bannerSubtitle} background={content.banner}/>
-        <Main>
-            <AboutUsShort content={content.shortAboutUs}/>
-            <AfdelingenMap afdelingen={props.afdelingen} compact/>
-            <div className="container flex sm:flex-row flex-col gap-8 mt-4">
-                <div className="basis-2/3">
-                    <Subheader>Laatste nieuws</Subheader>
-                    <EndlessPostsLoader postType={PostType.NEWS} posts={props.latestNews}/>
+
+    return (
+        <div>
+            <HeadPage
+                title="ROOD, Socialistische Jongeren"
+                description="Politieke organisatie voor socialistische of anderzijds linkse jongeren"
+                url="https://roodjongeren.nl/"
+            />
+            <Banner
+                title={content.bannerTitle}
+                subtitle={content.bannerSubtitle}
+                background={content.banner}
+            />
+            <Main>
+                <AboutUsShort content={content.shortAboutUs} />
+                <div className="md:max-w-screen-xl mx-auto flex flex-col md:flex-row gap-4">
+                    <div className="md:basis-8/12">
+                        <object
+                            data="/images/roodnederland.svg"
+                            type="image/svg+xml"
+                        />
+                    </div>
+                    <div className="md:basis-4/12">
+                        <AfdelingenTable afdelingen={props.afdelingen} />
+                    </div>
+                </div>
+                <div className="container flex sm:flex-row flex-col gap-8 mt-4">
+                    <div className="basis-2/3">
+                        <Subheader>Laatste nieuws</Subheader>
+                        <EndlessPostsLoader
+                            postType={PostType.NEWS}
+                            posts={props.latestNews}
+                        />
+                    </div>
+                    <div className="basis-1/3">
+                        <Subheader>Laatste inzendingen</Subheader>
+                        <EndlessPostsLoader
+                            postType={PostType.SUBMISSION}
+                            posts={props.latestSubmissions}
+                        />
+                    </div>
                 </div>
                 <div className="basis-1/3">
                     <Subheader>Laatste inzendingen</Subheader>
-                    <EndlessPostsLoader postType={PostType.SUBMISSION} posts={props.latestSubmissions}/>
+                    <EndlessPostsLoader
+                        postType={PostType.SUBMISSION}
+                        posts={props.latestSubmissions}
+                    />
                 </div>
-            </div>
-        </Main>
-    </div>;
+            </Main>
+        </div>
+    );
 }
 
 export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
@@ -51,16 +80,16 @@ export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
         fetchHome(),
         fetchAfdelingen(),
         fetchPosts(PostType.NEWS, null, null, 1, 6, true),
-        fetchPosts(PostType.SUBMISSION, null, null, 1, 6, true)
+        fetchPosts(PostType.SUBMISSION, null, null, 1, 6, true),
     ]);
-    
+
     return {
         props: {
             homeContent,
             afdelingen,
             latestNews: news.posts,
-            latestSubmissions: submissions.posts
+            latestSubmissions: submissions.posts,
         },
-        revalidate
+        revalidate,
     };
 }
