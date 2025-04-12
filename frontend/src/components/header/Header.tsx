7fda34993831727a1d4298cb37268c694ac2bbf6
Jelle Besseling <jelle@pingiun.com>
2025-04-12 17:12:23 +0200

    Add anti-navo-campagne link

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -82,6 +82,7 @@ export default function Header() {
                     >
                         Shop
                     </HeaderLink>
+                    <HeaderLink href="https://navoneedankje.nl">Anti-NAVO-Campagne</HeaderLink>
                     <HeaderLink
                         href="https://mijn.roodjongeren.nl"
                         externalLink
